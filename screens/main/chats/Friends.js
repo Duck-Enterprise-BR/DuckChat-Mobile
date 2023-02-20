f30480086a8684b8a7e63cbb9a9ec4067a07b484
Pato <vavizvini000@gmail.com>
2023-02-19 22:42:15 -0300

    Screen chat

---
+++ screens/main/chats/Friends.js
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+const Friends = () => {
+    return(
+        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+            <Text>Friends</Text>
+        </View>
+    );
+}
+
+export default Friends;
---
+++ styles/main/chat/ChatStyle.js
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native";
+import Colors from './../../../colors/Colors';
+
+const ChatStyle = StyleSheet.create({
+    tabBar: {
+        borderRadius: "50%",
+        width: "40%",
+        alignSelf: 'center',
+        marginVertical: "5%"
+    },
+
+});
+
+export default ChatStyle;
